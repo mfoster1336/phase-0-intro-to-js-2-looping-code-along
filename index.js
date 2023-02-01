bbc49762dd08b5027ed5dcb85e32dc1c530a6f6e
mikalfoster <mikalfoster36@gmail.com>
2023-01-31 20:12:31 -0600

    commit tree 308141ccf2de49234092dd643ef5e30aa5cd5ed4

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "Surprise";
+function writeCards(names, event) {
+    let completedCards = []
+    for (let i = 0; i < names.length; i++) {
+        completedCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        console.log(completedCards);
+    }
+    return completedCards;
+}
+
+writeCards(names, "surprise");
+
+
+function countDown(startingNumber) {
+    while (startingNumber >0 ){
+     console.log(startingNumber);
+     startingNumber -=1;
+    }
+    console.log (startingNumber)
+}
